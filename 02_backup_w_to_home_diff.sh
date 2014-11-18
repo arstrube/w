e587be5c8534e90be4f03848d1453660a91eea60
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-18 10:34:56 +0100

    intermediate

--- 02_backup_w_to_home_diff.sh
+++ 02_backup_w_to_home_diff.sh
@@ -24,29 +24,27 @@ function do_copy()
     exit;
 }
 
-# Change to the folder that this script resides in
-cd "`dirname "$0"`"
-
-basename=`basename $0 .sh`
-include=~/d/usr/tmp/$basename.txt
-exclude=$basename.txt
 
 # Copy files to backup drive, overwrite only if newer.
 # Copy target is w-drive-diff.
 # Copy only files not contained in W-DRIVE.
 printf '\n';
-printf 'B A C K I N G   U P   D I F F   O F   W - D R I V E \n'
-printf '=================================================== \n'
-printf 'Working... \n\n'
-echo excluding from: $exclude
-echo Including from: $include
-# set root=d:\dat
-# set cdrv=%cd:~0,2%
-# for /f "usebackq" %%a in (`dir * /b /on /ad`) do call :do_diff "%%a" /s & call :do_copy "%%a" /s
-# call :do_diff . & call :do_copy .
- rm -f $include
-# del /q %root%\W-DRIVE-DIFF\W-TEMP
-
+printf 'B A C K I N G   U P   D I F F   O F   W - D R I V E \n';
+printf '=================================================== \n';
+printf 'Working... \n\n';
+# Change to the folder that this script resides in
+cd "`dirname "$0"`";
+# Setup variables
+basename=`basename $0 _diff.sh`;
+src=`pwd`;
+dst=~/d/dat/w-drive-diff;
+include=~/d/usr/tmp/$basename.txt
+exclude=$basename.txt;
+echo excluding from: $exclude;
+echo Including from: $include;
+cmd="rsync -v --dry-run --exclude=* --include-from=$exclude $src $dst"
+echo "$cmd";
+$cmd;
 pause;
 
 
