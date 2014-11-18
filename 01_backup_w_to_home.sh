53c3d5dc2018f43f539cafbd2bf28ca4db13ba42
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-18 16:54:17 +0100

    slim them down a bit

--- 01_backup_w_to_home.sh
+++ 01_backup_w_to_home.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-function pause()
-{
-    read -n1 -r -p "Press any key to continue..." key;
-    printf "\n";
-}
+function pause() { read -n1 -rsp$'Press any key to continue...\n'; }
 
 # Change to the folder that this script resides in
 cd "`dirname "$0"`";
@@ -18,6 +14,4 @@ src=`pwd`/;
 dst=~/d/dat/w-drive;
 exclude=`basename $0 .sh`.txt; #or: exclude=${0%.txt}
 rsync -av --exclude-from=$exclude $src $dst;
-
 pause;
-
--- 01_backup_w_to_home_diff.sh
+++ 01_backup_w_to_home_diff.sh
@@ -1,54 +1,20 @@
 #!/bin/bash
 
-function pause()
-{
-    read -n1 -r -p"Press any key to continue..." key;
-    printf "\n";
-}
-
-# Copy files not contained in w-drive to backup drive,
-# e.g. w-drive-diff. Overwrite only if newer.
-printf '\n';
-printf 'B A C K I N G   U P   D I F F   O F   W - D R I V E \n';
-printf '=================================================== \n';
-printf 'Working... \n\n';
+function pause() { read -n1 -rsp$'Press any key to continue...\n'; }
 
 # Change to the folder that this script resides in
 cd "`dirname "$0"`";
 
-# Write files already in w-drive into temporary exclude file
-basename=`basename $0 .sh`;
-exclude=~/d/usr/tmp/$basename.txt;
-bak=~/d/dat/w-drive;
-find $bak/ -type f | sed "s:$bak::" > $exclude; # or: tree -if --noreport $bak/
-
-# Run the differential backup
+# Copy files not in w-drive to w-drive-diff, overwrite only if newer
+printf '\n';
+printf 'B A C K I N G   U P   D I F F   O F   W - D R I V E \n';
+printf '=================================================== \n';
+printf 'Working... \n\n';
 src=`pwd`/;
 dst=~/d/dat/w-drive-diff;
+exclude=~/d/usr/tmp/`basename $0 _diff.sh`.txt;
+bak=~/d/dat/w-drive;
+find $bak/ -type f | sed "s:$bak::" > $exclude; # or: tree -if --noreport $bak/
 rsync -av --exclude='.*' --exclude-from=$exclude $src $dst;
 
 pause;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
