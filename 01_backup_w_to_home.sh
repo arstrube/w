516be01a824c5a2ba156a28e6c38cf32a2075e52
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-18 10:27:39 +0100

    Improve syntax a little

--- 01_backup_w_to_home.sh
+++ 01_backup_w_to_home.sh
@@ -7,19 +7,20 @@ function pause()
 }
 
 # Change to the folder that this script resides in
-cd "`dirname "$0"`"
+cd "`dirname "$0"`";
 
 # Copy files to backup drive, overwrite only if newer.
 printf '\n';
-printf 'B A C K I N G   U P   W - D R I V E \n'
-printf '=================================== \n'
-printf 'Working... \n\n'
-src=`pwd`
-dst=~/d/dat/w-drive
-exclude=`basename $0 .sh`.txt
-#exclude=${0%.txt}
-echo rsync -av --exclude-from=$exclude $src/ $dst;
-rsync -av --exclude-from=$exclude $src/ $dst;
+printf 'B A C K I N G   U P   W - D R I V E \n';
+printf '=================================== \n';
+printf 'Working... \n\n';
+src=`pwd`;
+dst=~/d/dat/w-drive;
+exclude=`basename $0 .sh`.txt;
+# exclude=${0%.txt}
+cmd="rsync -av --exclude-from=$exclude $src/ $dst";
+echo $cmd;
+$cmd;
 
 pause;
 
